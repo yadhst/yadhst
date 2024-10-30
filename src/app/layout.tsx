87bc3fa9f135f0e44b8a80d921480cef269c1458
yadhst <fayyadh.abdurrahman577@gmail.com>
2024-10-30 20:58:10 +0700

    #FreePalestine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="en" id="FreePalestine" suppressHydrationWarning>
             <body
                 className={cn(inter.variable, "relative font-sans antialiased")}
             >
